289ace3b6c3cfc4a64121994e778dd7b70ef2642
AmeerAlkholany <ameeralkholany@gmail.com>
2025-05-02 17:17:50 +0300

    Creating Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import GameCard from './components/GameCard';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
    <NavBar/>
+   <GameCard/>
     </>
   )
 }
